cd8f508d1a8e583a68805895e15dbeaa8078015e
V-Pradhan <pradhanvinayak5@gmail.com>
2026-01-01 22:10:34 +0530

    Codeforces 71A

---
+++ waytoolongwords.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class waytoolongwords {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i=1;i<=n;i++){
+            String word = sc.next();
+            if(word.length()<=10){
+                System.out.println(word);
+            }
+            else{
+                System.out.println(""+ word.charAt(0)+ (word.length()-2) + word.charAt((word.length()-1)));
+            }
+        }
+    }
+    
+}
