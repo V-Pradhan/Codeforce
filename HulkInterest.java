883606c03608d9c916f9349f5f68f1cb2d827209
V-Pradhan <pradhanvinayak5@gmail.com>
2026-02-03 18:36:54 +0530

    DAY 32

---
+++ HulkInterest.java
@@ -0,0 +1,31 @@
+ import java.util.Scanner;
+
+public class HulkInterest {
+   
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 1; i <= n; i++) {
+
+            if (i % 2 == 1) {
+                result.append("I hate");
+            } else {
+                result.append("I love");
+            }
+
+            if (i == n) {
+                result.append(" it");
+            } else {
+                result.append(" that ");
+            }
+        }
+
+        System.out.println(result.toString());
+    }
+
+}
